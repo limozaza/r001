120217724f9336426ef1cdda1031577498ad63ae
Zakaria <zakboite@gmail.com>
2018-09-04 16:14:05 +0200

    understand state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+ 
+  state = {
+    persons: [
+      {name: "Zakaria", age: 32},
+      {name: "Sofia", age: 31},
+      {name: "Kassym", age: 3},
+      {name: "Malika", age: 1},
+      {name: "AAA", age: 32},
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
-        <Person name="Zakaria" age="32">My Hobbies: Nada</Person>
-        <Person name="Sofia" age="31"/>
-        <Person name="Kassym" age="3"/>
-        <Person name="Malika" age="1"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Nada</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+        <Person name={this.state.persons[4].name} age={this.state.persons[4].age}/>
+        
       </div>
     );
   }
