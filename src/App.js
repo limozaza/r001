d73feb867cae402add4b726ea8cb28cef12eb92a
Zakaria <zakboite@gmail.com>
2018-09-04 16:21:01 +0200

    Handling Evenet with methods

--- src/App.js
+++ src/App.js
@@ -13,11 +13,16 @@ class App extends Component {
       {name: "AAA", age: 32},
     ]
   }
+
+  switchNameHandler = () => {
+    console.log('click')
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Nada</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
